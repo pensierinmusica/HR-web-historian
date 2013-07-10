8ce278fb958e583587a3720adb743eff904a9506
Hack Reactor Students <github@hackreactor.com>
2013-07-10 12:34:34 -0700

    Server receives POST request from client form

--- web/basic-server.js
+++ web/basic-server.js
@@ -3,7 +3,9 @@ var myStuff = require("./request-handler");
 
 var port = 8080;
 var ip = "127.0.0.1";
-var server = http.createServer(myStuff.handleRequest);
+var server = http.createServer(function(req,res) {
+  myStuff.handleRequest(req,res);
+});
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,34 @@
+// New version
 var path = require('path');
 module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
 
+
+// Old version (check if it needs to be removed)
+var url = require('url');
+exports.datadir = __dirname + "data/sites.txt"; // tests will need to override this.
+
+
 module.exports.handleRequest = function (req, res) {
   console.log(exports.datadir);
+
+  var path = url.parse(req.url).path;
+
+  if (path === '/') {
+    if (req.method === 'POST') {
+      req.setEncoding('utf8');
+      req.on('data', function(data) {
+        data = data.replace(/^url=/, '');
+        console.log(data);
+      });
+    }
+    require('fs').readFile('./public/index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+    });
+  } else if (path.match(/\.css$/)) {
+      require('fs').readFile('./public' + path, function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/css'});
+      res.end(data);
+    });
+  }
 };
