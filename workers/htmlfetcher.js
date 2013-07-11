2c40a4da544b529e2314512dbab70056c306cee1
Hack Reactor Students <github@hackreactor.com>
2013-07-10 17:30:47 -0700

    Implements worker that fetches html

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,6 @@
 // eventually, you'll have some code here that uses the tested helpers 
 // to actually download the urls you want to download.
+var fetcher = require('./lib/html-fetcher-helpers.js');
+var absPath = '/Users/hackreactor/code/maxmalin/2013-06-web-historian/';
+
+fetcher.readUrls(absPath + 'data/sites.txt', fetcher.downloadUrls);
--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,7 +1,33 @@
-exports.readUrls = function(filePath, cb){
+var http = require('http-get');
+var fs = require('fs');
+var absPath = '/Users/hackreactor/code/maxmalin/2013-06-web-historian/';
 
+exports.readUrls = function(filePath, cb){
+  fs.readFile(filePath, 'utf8', function(err, data) {
+    console.log('URLs list start: ' + JSON.stringify(data));
+    var urls = data.split('\n');
+    console.log('URLs list mid: ' + JSON.stringify(urls));
+    for (var i = 0; i < urls.length; i++) {
+      console.log(fs.existsSync(absPath + 'data/sites/' + urls[i]));
+      fs.existsSync(absPath + 'data/sites/' + urls[i]) && (urls[i] = 0);
+    }
+    urls.pop();
+    console.log('URLs list end: ' + JSON.stringify(urls));
+    cb(urls);
+  });
 };
 
 exports.downloadUrls = function(urls){
-  // fixme
+  for (var i = 0; i < urls.length; i++) {
+    if (urls[i]) {
+      var options = {url: urls[i]};
+      http.get(options, absPath + 'data/sites/' + urls[i], function (error, result) {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log('File downloaded at: ' + result.file);
+        }
+      });
+    }
+  }
 };
